55becba99ea2911a8fedf9c270e095cc47062a6c
msalad <msalad@uncc.edu>
2017-08-31 20:31:32 +0000

    add

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -9,21 +9,6 @@ bool checkIfThrees (char *ttt,int a,int  b, int c);
 bool checkIfWinner (char *ttt);
 
 
-/*
-How games can end with a winner
-0,1,2
-3,4,5
-6,7,8
-
-2,4,6
-0,4,8
-
-0,3,6
-1,4,7
-2,5,8
-
-
-*/
 
 int main() {
 
@@ -31,19 +16,22 @@ int main() {
 					// in your tic-tac-toe square; assume a default character
 					// like '-' or something
 	char tic_tac_toe[9] = {'-','-','-','-','-','-','-','-','-'};
-	
-	//char ttt[9] = tic_tac_toe;
 
 					// draw the array
 	draw(&tic_tac_toe[0]);
 
 	playTicTacToe(&tic_tac_toe[0]);	
-
+    //char winner;
 	return 0;
 }
 
 void playTicTacToe (char *ttt) {
-    for(int i = 0; i < 9; i++){
+    //variable that keeps track of whose turn it is
+    int i = 0;
+    int spots = 0;
+    int validMoves = 0;
+    //Loop through 
+    while(!checkIfWinner(ttt) && spots < 9){
          int num;
         if(i % 2 == 0){
         cout << "Player 1's turn, where would you like to place X (0-8)? " << endl;
@@ -53,7 +41,7 @@ void playTicTacToe (char *ttt) {
         cout << "Player 2's turn, where would you like to place 0 (0-8)? " << endl;
         cin >> num;
         }
-        if(num > -1 && num < 9){
+        if(num > -1 && num < 9  && ttt[num] == '-' ){
             if(i % 2 == 0){
                 ttt[num] = 'X';
             }
@@ -61,35 +49,27 @@ void playTicTacToe (char *ttt) {
                  ttt[num] = '0';
 
             }
-			if(checkIfWinner(ttt)){
-			    cout << "winner found" << endl;
-			}
+			        
 			        draw(ttt);
+			        //if(checkIfWinner(ttt)){
+			            //break;
+			           
+			        //}
+			        spots++;
         }
         else{
-            cout << " You lost your turn for entering number out range! ";
+            cout << " You lost your turn for entering number out range! or entering a postion thats already taken " << num << " ";
         }
+        i++;
+         if(spots == 9){
+    cout << "Game ended in a tie" << endl;
+    break;
     }
+}
     //Check if value is between 1-9
     //Check if there isn't already a spot there, if there is u lose your turn
     //Check if user has won
-    
-    
-			// this function contains  the game logic
-			// assume a move by player A, followed by player B
-			// after each move, you must check if the respective player
-			// has won, in which case the game is complete; use a separate
-			// function to accomplish this.
-
-			// players will enter the position where they want to enter in
-			// the tic-tac-toe square. Assume the numbering from 1 to 9 
-			// in row-major order 
-
-			// use cin and cout for input and output
-// 			ttt[3] = 'X';
-// 			ttt[4] = 'X';
-// 			ttt[5] = 'X';
-			
+   
 
 }
 
@@ -132,7 +112,10 @@ return false;
 bool checkIfThrees(char* ttt, int a, int b, int c){
     if(ttt[a] == ttt[b] && ttt[b] == ttt[c] && ttt[a] != '-' && ttt[b] != '-' && ttt[c] != '-'){
         
-        cout << "true";
+        //cout << "true";
+        //if(checkIfWinner(ttt)){
+			    cout <<"Player " << ttt[a] << " wins" << endl;
+		//	}
         return true;
     }
     return false;
@@ -152,6 +135,5 @@ cout << endl;
 
 // This function determines if the game has ended - either a row, column or
 // a diagonal should be filled with the same character for it to be complete.
-
 bool gameComplete(char *ttt) {
 }
