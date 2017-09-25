150af751154d8353cbfd3e56134d4b09a55351ca
msalad <msalad@uncc.edu>
2017-09-25 00:01:29 +0000

    working on the next part

--- Mountain_Paths_P3/main.cpp
+++ Mountain_Paths_P3/main.cpp
@@ -180,91 +180,93 @@ std::string filename = "";
 
 using namespace std;
 
-void greed(int index, int arr[], int height) {
 
-	for (int z = 0; z< height; z++) {
-		int up = index - height - 1;
-		int middle = index + 1;
-		int down = index + height + 1;
+void greed2(int index, int** hold, int height) {
+	int inner = 0;
+	while (inner < height) {
+		//for (int j = 0; j < height; j++) {
+			//Gives us the pointer to the row beginning
+			//int* currentRow = hold[j];
+
+			//for (int k = 0; k < width; k++) {
+		cout << endl << " starting at index =  " << index << " inner = " << inner;
+		int* upRow = &hold[index - 1][inner + 1];
+		int* midRow = &hold[index][inner + 1];
+		int* downRow = &hold[index + 1][inner + 1];
 
 
-		if (up < 0) {
-			if (arr[middle] <= arr[down]) {
-				arr[index] = 0;
-				index += 1;
-				cout << endl << " going mid , up is less than 0";
-			}
-			else {
-
-				arr[index] = 0;
-				index += (height + 1);
-				cout << endl << " going down ";
-			}
+		if (*upRow < *midRow) {
+			hold[index - 1][inner + 1] = 0;
+			index--;
+			inner++;
 		}
 
-		else if (down >= width*height) {
-			if (arr[middle] <= arr[up]) {
-				arr[index] = 0;
-				index += 1;
-				cout << endl << " going mid, down is greater than width* height ";
-			}
-			else {
-				arr[index] = 0;
-				index -= (height - 1);
-				cout << endl << " going up, down is greater than width*height";
-			}
-		}
 
-		else if (arr[up] <= arr[middle]) {
-			if (arr[up] <= arr[down]) {
-				arr[index] = 0;
-				index -= (height - 1);
+		if (*upRow <= *midRow) {
+			if (*upRow <= *downRow) {
+				hold[index - 1][inner + 1] = 0;
+				index--;
+				inner++;
 				cout << endl << " going up ";
 			}
 
 			else {
-				arr[index] = 0;
-				index += (height + 1);
+				hold[index + 1][inner + 1] = 0;
+				index++;
+				inner++;
 				cout << endl << " going down ";
 			}
 		}
 
 
-		else if (arr[middle] <= arr[down]) {
-			if (arr[middle] <= arr[up]) {
-				arr[index] = 0;
-				index += 1;
+		else if (*midRow <= *downRow) {
+			if (*midRow <= *upRow) {
+				hold[index][inner + 1] = 0;
+				inner++;
 				cout << endl << " going mid ";
 			}
 			else {
-				arr[index] = 0;
-				index -= (height - 1);
+				hold[index - 1][inner + 1] = 0;
+				index--;
+				inner++;
 				cout << endl << " going up ";
 			}
 		}
 
-		else if (arr[down] <= arr[up]) {
-			if (arr[down] <= arr[middle]) {
-				arr[index] = 0;
-				index += (height + 1);
+		else if (*downRow <= *upRow) {
+			if (*downRow <= *midRow) {
+				hold[index + 1][inner + 1] = 0;
+				index++;
+				inner++;
 				cout << endl << " going down ";
 			}
 			else {
-				arr[index] = 0;
-				index += 1;
+				hold[index][inner + 1] = 0;
+				inner++;
 				cout << endl << " going mid ";
 			}
 		}
+		delete[] upRow, midRow, downRow;
 	}
+		//}
+	//}
+
 
 
 }
-void writeToFile(int h, int w, int arr[]) {
+void writeToFile(int h, int w, int** hold) {
 	std::filebuf res;
 	res.open("out.ppm", std::ios::out);
 	std::ostream output(&res);
 	output << "P2\n" << h << " " << w << "\n" << maximum << "\n";
-	for (int i = 0; i<((h*w)); i++) {
+
+	for (int outer = 0; outer < width; outer++) {
+		for (int inner = 0; inner < height; i++) {
+			output << hold[outer][inner] << " ";
+		}
+		output << "\n" << " ";
+	}
+	/*for (int i = 0; i<((h*w)); i++) {
 		if (i%h == 0 && i != 0) {
 			output << "\n" << arr[i] << " ";
 		}
@@ -273,7 +275,7 @@ void writeToFile(int h, int w, int arr[]) {
 
 		}
 
-	}
+	}*/
 	res.close();
 }
 
@@ -284,11 +286,21 @@ int main()
 	string line;
 	int value = 0;
 	std::string::size_type sz;
+	int numOfRowsToPathFind = 0;
 
 
-	cout << "Which index do u want to start from" << endl;
-	cin >> row;
-	cout << "Enter the name of the file";
+
+	cout << "How many rows do u want to path find? " << endl;
+	cin >> numOfRowsToPathFind;
+	int* rows = new int[numOfRowsToPathFind];
+
+	for(int c= 0; c < numOfRowsToPathFind; c++ ){
+		cout << endl << "Enter the row to start from for row " << c <<endl;
+		cin >> rows[c];
+	}
+
+	//cin >> row;
+	cout << "Enter the name of the file"<<endl;
 	cin >> filename;
 	filename += ".dat";
 	//std::string totalpath = "H:\\Documents\\Visual Studio 2015\\Projects\\Assignment2-3116\\";
@@ -308,7 +320,7 @@ int main()
 	}
 	int holderPos = 0;
 	int positionOfRow = 0;
-	
+
 	//int arr[n1*10];
 	arr = new int[height*width];
 	while (myFile >> z) {
@@ -331,8 +343,17 @@ int main()
 	myFile.close();
 
 	for (int z = 0; z < height; z++) {
-		cout << endl << " the row pointer for row " << z << " is " << holder[z] << " it's corresponding value is "<<*holder[z] << " the value after this is " <<holder[z][1];
+		//cout << endl << " the row pointer for row " << z << " is " << holder[z] << " it's corresponding value is " << *holder[z] << " the value after this is " << holder[z][1];
+		row = rows[z];
+		//row = ((row*width) - (width - 1)) - 1;
+		greed2(row, holder, height);
+
 	}
+	writeToFile(height, width, holder);
+	delete[] holder, rowBeginning;
+
+
+	
 
 	//convert row to specifici spot
 	//row = ((row*width) - (width - 1)) - 1;
